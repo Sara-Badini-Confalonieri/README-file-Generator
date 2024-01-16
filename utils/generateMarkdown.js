1025263e4a01a31236abb91c3fe1894c2f4c83f9
Sara Badini Confalonieri <sara.badiniconfalonieri@gmail.com>
2024-01-16 14:47:00 +0000

    created basic README file layout in generateMarkdown.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,46 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+
+  ${data.description}
+
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any question, feel free to contact me! 
+  Email: ${data.email}
+  GitHub: [${data.username}](https://github.com/${data.username})
+
+  `;
 }
 
 module.exports = generateMarkdown;
